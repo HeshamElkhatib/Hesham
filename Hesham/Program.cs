05ac828467f5915f6791741b1af763659cf1e449
HeshamElkhatib <hesham.elkhatib@hotmail.com>
2017-04-14 19:38:32 +0200

    Add Corporation Database class Add RealEstateAgency class

--- Hesham/Program.cs
+++ Hesham/Program.cs
@@ -3,10 +3,53 @@ using Starcounter;
 
 namespace Hesham
 {
+    [Database]
+    public class Corporation
+    {
+        public string Name;
+        public RealEstateAgency Agency;
+    }
+    [Database]
+    public class RealEstateAgency
+    {
+        public QueryResultRows<Corporation> Corporations => Db.SQL<Corporation>("SELECT c FROM Hesham.Corporation WHERE c.Agency = ?", this);
+    }
     class Program
     {
         static void Main()
         {
+            Application.Current.Use(new HtmlFromJsonProvider());
+            Application.Current.Use(new PartialToStandaloneHtmlProvider());
+
+            Db.Transact(() =>
+            {
+                var mainAgency = Db.SQL<RealEstateAgency>("SELECT a FROM RealEstateAgency a").First;
+                if(mainAgency == null)
+                {
+                    new RealEstateAgency();
+                }
+            });
+            Handle.GET("/Hesham", () =>
+            {
+                return Db.Scope(() =>
+                {
+                    var mainAgency = Db.SQL<RealEstateAgency>("SELECT a FROM RealEstateAgency a").First;
+                    var json = new RealEstateAgencyJson { Data = mainAgency };
+                    if (Session.Current == null)
+                    {
+                        Session.Current = new Session(SessionOptions.PatchVersioning);
+                    }
+                    json.Session = Session.Current;
+                    return json;
+                });
+                
+            });
+            Handle.GET("/Hesham/partial/corporation/{?}", (string id) =>
+            {
+                var json = new CorporationJson();
+                json.Data = DbHelper.FromID(DbHelper.Base64DecodeObjectID(id));
+                return json;
+            });
 
         }
     }
