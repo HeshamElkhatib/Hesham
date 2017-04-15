8320a5f17dbd629eafd5bb5a3455b6135167eba3
HeshamElkhatib <hesham.elkhatib@hotmail.com>
2017-04-15 20:00:03 +0200

    Add DataSeeder Add Requests Handler

---
+++ Hesham/DataSeeder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Starcounter;
+namespace Hesham
+{
+    public class DataSeeder
+    {
+        public void Seed()
+        {
+            Db.Transact(() =>
+            {
+                var mainAgency = Db.SQL<Models.RealEstateAgency>("SELECT a FROM RealEstateAgency a").First;
+                if (mainAgency == null)
+                {
+                    new Models.RealEstateAgency();
+                }
+                var f = Db.SQL<Models.Franchise>("SELECT f FROM Franchise f").First;
+                if (f == null)
+                {
+                    new Models.Franchise();
+                }
+
+            });
+        }
+    }
+}
---
+++ Hesham/RequestsHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Starcounter;
+namespace Hesham
+{
+    public class RequestsHandler
+    {
+        public void AddHandlers()
+        {
+            Handle.GET("/Hesham", () =>
+            {
+                return Db.Scope(() =>
+                {
+                    var mainAgency = Db.SQL<Models.RealEstateAgency>("SELECT a FROM RealEstateAgency a").First;
+                    var json = new RealEstateAgencyJson { Data = mainAgency };
+                    if (Session.Current == null)
+                    {
+                        Session.Current = new Session(SessionOptions.PatchVersioning);
+                    }
+                    json.Session = Session.Current;
+                    return json;
+                });
+
+            });
+            Handle.GET("/Hesham/partial/corporation/{?}", (string id) =>
+            {
+                var json = new CorporationJson();
+                json.Data = DbHelper.FromID(DbHelper.Base64DecodeObjectID(id));
+                return json;
+            });
+
+            Handle.GET("/Hesham/franchise/{?}", (string id) =>
+            {
+                return Db.Scope(() =>
+                {
+                    var franchise = new FranchiseJson();
+                    franchise.Data = DbHelper.FromID(DbHelper.Base64DecodeObjectID(id));
+                    if (Session.Current == null)
+                    {
+                        Session.Current = new Session(SessionOptions.PatchVersioning);
+                    }
+                    franchise.Session = Session.Current;
+
+                    return franchise;
+                });
+
+            });
+            Handle.GET("/Hesham/partial/franchise/{?}", (string id) =>
+            {
+                var franchise = new FranchiseJson();
+                franchise.Data = DbHelper.FromID(DbHelper.Base64DecodeObjectID(id));
+                return franchise;
+            });
+        }
+    }
+}
