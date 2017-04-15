46a565b6d769b4e4ff86a0be6513f0bba070f941
HeshamElkhatib <hesham.elkhatib@hotmail.com>
2017-04-15 20:02:46 +0200

    Move Handlers and seeding logic from Main

--- Hesham/Program.cs
+++ Hesham/Program.cs
@@ -1,28 +1,10 @@
 ﻿using System;
 using Starcounter;
+using System.Collections;
+using Hesham.Models;
 
 namespace Hesham
 {
-    [Database]
-    public class Corporation
-    {
-        public string Name;
-        public RealEstateAgency Agency;
-    }
-    [Database]
-    public class RealEstateAgency
-    {
-        public QueryResultRows<Corporation> Corporations => Db.SQL<Corporation>("SELECT c FROM Hesham.Corporation WHERE c.Agency = ?", this);
-    }
-    [Database]
-    public class Franchise
-    {
-        public string Name;
-        public string Street, ZipCode, City, Country, Number;
-        public string Address => Street + " " + Number + ", " + ZipCode + " " + City + ", " + Country;
-    }
-    
-   
     class Program
     {
         static void Main()
@@ -30,54 +12,11 @@ namespace Hesham
             Application.Current.Use(new HtmlFromJsonProvider());
             Application.Current.Use(new PartialToStandaloneHtmlProvider());
 
-            Db.Transact(() =>
-            {
-                var mainAgency = Db.SQL<RealEstateAgency>("SELECT a FROM RealEstateAgency a").First;
-                if(mainAgency == null)
-                {
-                    new RealEstateAgency();
-                }
-                
-            });
-            Handle.GET("/Hesham", () =>
-            {
-                return Db.Scope(() =>
-                {
-                    var mainAgency = Db.SQL<RealEstateAgency>("SELECT a FROM RealEstateAgency a").First;
-                    var json = new RealEstateAgencyJson { Data = mainAgency };
-                    if (Session.Current == null)
-                    {
-                        Session.Current = new Session(SessionOptions.PatchVersioning);
-                    }
-                    json.Session = Session.Current;
-                    return json;
-                });
-                
-            });
-            Handle.GET("/Hesham/partial/corporation/{?}", (string id) =>
-            {
-                var json = new CorporationJson();
-                json.Data = DbHelper.FromID(DbHelper.Base64DecodeObjectID(id));
-                return json;
-            });
+            DataSeeder seeder = new DataSeeder();
+            seeder.Seed();
 
-            Handle.GET("/Hesham/franchise/{?}", (string id) =>
-            {
-                return Db.Scope(() =>
-                {
-                    var franchise = new FranchiseJson();
-                    franchise.Data = DbHelper.FromID(DbHelper.Base64DecodeObjectID(id));
-                    if (Session.Current == null)
-                    {
-                        Session.Current = new Session(SessionOptions.PatchVersioning);
-                    }
-                    franchise.Session = Session.Current;
-                    
-                    return franchise;
-                });
-                
-            });
-
+            RequestsHandler requestsHandler = new RequestsHandler();
+            requestsHandler.AddHandlers();
         }
     }
 }
